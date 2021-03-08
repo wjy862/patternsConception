561ce48a6519f375181aff4f774bd86b00a68db4
leowj <leowjy123456@gmail.com>
2021-03-09 00:09:48 +0100

    Excercice 4.1 creation du class Personnel

---
+++ src/main/java/fr/uvsq/patterns/organisation/Personnel.java
@@ -0,0 +1,17 @@
+package fr.uvsq.patterns.organisation;
+
+import java.util.Date;
+
+public class Personnel {
+    String nom;
+    String prenom;
+    Date dateNaissance;
+    String[] telephone;
+
+    public void poste(){
+        /*son poste */
+    }
+    public void mission(){
+        /*sa mission */
+    }
+}
